251eec21837d446e12cc79ce82a56b2cbf65a6a4
blackkakapo <petrachialexandru.gmail.com>
2022-02-25 14:41:00 +0200

    v0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='dexonlineBK',
-	version='0.0.1',
+	version='0.0.4',
 	description='Small dexonline.ro API for Python',
 	author='Alexandru Petrachi (BlackKakapo)',
 	packages=['dexonlineBK'],
